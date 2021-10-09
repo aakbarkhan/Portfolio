d6f19617fb0922f32aa78875d20717af87418854
aakbarkhan <aakuukhan@gmail.com>
2021-10-09 13:56:45 +0530

    Add validation js

---
+++ validation.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#contact-form');
+const warningtxt = document.querySelector('#warningtxt');
+ 
+
+form.addEventListener('submit', (event) => {
+  const emailstring = document.getElementById('email').value;
+
+  if (emailstring !== emailstring.toLowerCase()) {
+    warningtxt.innerHTML = 'Please fill out the email with lower case only, thank you.';
+    event.preventDefault();
+  }
+});
