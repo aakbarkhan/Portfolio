404ad6f0d5fcfe7eab5c9dac06f723ca58689908
aakbarkhan <aakuukhan@gmail.com>
2021-10-09 11:08:14 +0530

    Add mobile and desktop popup

--- script.js
+++ script.js
@@ -25,39 +25,144 @@ menuItems.forEach(
 )
 
 
-const data = 
+// mobile popup
+const data = [
   {
-  theme: "Multi-Post Stories",
-  button: "X",
-  text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
-  html: "html",
-  ruby:"Ruby on rails",
-  css: "css",
-  live: "See Live",
-  source: "Source Code"
-};
-// create a section
-// const mode = document.querySelector('.model');
-// // create a div
-// const workDiv = document.createElement('div')
-// workDiv.classList.add('work')
-// // create a textnode
-// const heading = document.createElement('h2');
-// const text = document.createTextNode(data.theme)
-// console.log(text)
-// heading.id = 'js-post-heading';
-// console.log(document.getElementById("js-post-heading"));
+    title: 'Multi-Post stories',
+    image:'assets/SnapshootPortfolio.png',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing\
+      elit, sed do eiusmod tempor incididunt ut labore et dolore \
+      magna aliqua. Sem et tortor consequat id porta nibh. Velit \
+      aliquet sagittis id consectetur. Urna id volutpat lacus laoreet.\
+      Fames ac turpis egestas integer eget aliquet nibh praesent \
+      tristique.',
+    languages:['html','css','javaScript','github','Ruby on rails','Bootstrap'],
+    live : 'See Live',
+    source: 'See Source' 
+  },
+  {
+    title: 'Google',
+    image:'assets/SnapshootPortfolio.png',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing\
+      elit, sed do eiusmod tempor incididunt ut labore et dolore \
+      magna aliqua. Sem et tortor consequat id porta nibh. Velit \
+      aliquet sagittis id consectetur. Urna id volutpat lacus laoreet.\
+      Fames ac turpis egestas integer eget aliquet nibh praesent \
+      tristique.',
+    languages:['html','css','javaScript','github','Ruby on rails','Bootstrap'],
+    live : 'See Live',
+    source: 'See Source' 
+  },
+  {
+    title: 'Facebook',
+    image:'assets/SnapshootPortfolio.png',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing\
+      elit, sed do eiusmod tempor incididunt ut labore et dolore \
+      magna aliqua. Sem et tortor consequat id porta nibh. Velit \
+      aliquet sagittis id consectetur. Urna id volutpat lacus laoreet.\
+      Fames ac turpis egestas integer eget aliquet nibh praesent \
+      tristique.',
+    languages:['html','css','javaScript','github','Ruby on rails','Bootstrap'],
+    live : 'See Live',
+    source: 'See Source' 
+  },
+  {
+    title: 'Portfolio',
+    image:'assets/SnapshootPortfolio.png',
+    text:'Lorem ipsum dolor sit amet, consectetur adipiscing\
+      elit, sed do eiusmod tempor incididunt ut labore et dolore \
+      magna aliqua. Sem et tortor consequat id porta nibh. Velit \
+      aliquet sagittis id consectetur. Urna id volutpat lacus laoreet.\
+      Fames ac turpis egestas integer eget aliquet nibh praesent \
+      tristique.',
+    languages:['html','css','javaScript','github','Ruby on rails','Bootstrap'],
+    live : 'See Live',
+    source: 'See Source' 
+  }
+]
 
-// heading.appendChild(text);
-// workDiv.appendChild(heading);
-// // aapend the div in the mode section
-// mode.appendChild(workDiv);
-// // append the mode in the body document.
-// // mode.style.display = 'none';
-// document.getElementById("js-post-heading").style.color = 'black'
 
-// document.body.appendChild(mode)
+  function cardGenerate(data) {
+    const page = document.createElement('div');
+    page.className = 'modelPage';
+    let num = data.length;
+    let i = Math.floor(Math.random() * num);
+    // console.log(i);
+    
+           
+        const title = document.createElement('h2');
+        title.className = 'port-title'
+        const span = document.createElement('button');
+        span.className = "closebtn"
+        const img = document.createElement('img');
+        const para = document.createElement('p');
+        const lang = document.createElement('div');
+        lang.className ='frameworks'
+        const ul = document.createElement('ul');
+        ul.className = 'lang-pop';
+        const list1 = document.createElement('li');
+        list1.className ='add-border';
+        const list2 = document.createElement('li');
+        list2.className ='add-border';
+        const list3 = document.createElement('li');
+        list3.className ='add-border';
 
-// console.log(mode)
+        const btnnice = document.createElement('div');
+        btnnice.className = 'live-source';
+        const btn1 =  document.createElement('button');
+        btn1.className = 'live';
+        
+        const btn2 =  document.createElement('button');
+        btn2.className = 'source';
+        
+          page.appendChild(title);
+          page.appendChild(span);
+          page.appendChild(img);
+          page.appendChild(para);
+          page.appendChild(lang);
+          lang.appendChild(ul);
+          ul.appendChild(list1);
+          ul.appendChild(list2);
+          ul.appendChild(list3);
+          btnnice.appendChild(btn1);
+          btnnice.appendChild(btn2);
+          page.appendChild(btnnice);
+          
+          img.src = data[i].image;
+          img.className = 'project-image';
+          title.innerText = data[i].title;
+          span.innerText = 'x';
+    
+          para.innerText = data[i].text;
+          para.className = 'para1'
+          list1.innerText = data[i].languages[0];
+          list2.innerText = data[i].languages[2];
+          list3.innerText = data[i].languages[3];
+          btn1.innerHTML =`${data[i].live}` + '<img id="live-btn" src="./live.png" alt="btn">';
+          btn2.innerHTML = `${data[i].source}` +'<i id="github" class="fab fa-github"></i>';
+          const mainContent = document.querySelector('.main');
+    
+          return mainContent.appendChild(page);
+      
+  }
+  
+  const btnn = document.querySelectorAll('.btn');
+  const model = document.querySelector('.page');
+  function popUp() {    
+    document.body.appendChild(cardGenerate(data));
+    
+    let  del = document.querySelector('.modelPage');
+    let  closeBtn = document.querySelector('.closebtn');
+    document.querySelector('#bg-nice').classList.add('blurred');
+    closeBtn.addEventListener('click', ()=> {
+      document.body.removeChild(del);
+      document.querySelector('#bg-nice').classList.remove('blurred');
+      })
+  }
+  
+  var btnsMobileArr = Array.from(btnn);
 
-
+  btnsMobileArr.forEach((btn, idx) => {
+    btn.setAttribute('id', idx);
+    btnn[idx].addEventListener('click',popUp );
+  })
